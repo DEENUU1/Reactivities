5a8a1100e962b0e4974d04e9ecbd54256312dae7
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-10 16:16:47 +0200

    Section 10 | Error handling and validation

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -12,12 +13,15 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,7 @@
-﻿using AutoMapper;
+﻿using Application.Core;
+using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 using System;
@@ -12,32 +14,44 @@ namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
-            public Activity Activity
+            public Activity Activity { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
             {
-                get; set;
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
             }
+        }
 
-            public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
             {
-                private readonly DataContext _context;
-                private readonly IMapper _mapper;
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
-                public Handler(DataContext context, IMapper mapper)
-                {
-                    _context = context;
-                    _mapper = mapper;
-                }
-                public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
-                {
-                    var activity = await _context.Activities.FindAsync(request.Activity.Id);
-                    _mapper.Map(request.Activity, activity);
+                if (activity == null) return null;
 
-                    await _context.SaveChangesAsync();
-                    return Unit.Value;
-                }
+                _mapper.Map(request.Activity, activity);
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to update activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
+  
 }
