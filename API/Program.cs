b6dc9195157e87fc49503a5bf8cc3557589d3e8b
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-10 20:57:56 +0200

    Section 12 | Identity

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
