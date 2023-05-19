f57860b54015661f0ba2dc8ad9c2c556cdabd55d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-19 20:27:14 +0200

    End of the section 17, working on the API Image upload

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Core;
+﻿using API.Extensions;
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,17 +11,22 @@ namespace API.Controllers
     {
         private IMediator _mediator;
 
-        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
-
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>();
 
         protected ActionResult HandleResult<T>(Result<T> result)
         {
             if (result == null) return NotFound();
-            if(result.IsSuccess && result.Value != null) 
+
+            if (result.IsSuccess && result.Value != null)
                 return Ok(result.Value);
+
             if (result.IsSuccess && result.Value == null)
                 return NotFound();
+
             return BadRequest(result.Error);
         }
+
+
     }
 }
